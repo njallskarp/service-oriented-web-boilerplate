ac44ded6431a10f46d2c0af6b8fb22b3fd7b6fa4
njallskarp <njallis@gmail.com>
2024-02-19 09:43:29 -0800

    fixed failing test

--- rest-api/src/environment/environment.unit.test.ts
+++ rest-api/src/environment/environment.unit.test.ts
@@ -3,6 +3,6 @@ import { expect } from 'chai';
 describe('Example Test', () => {
 	it('should pass this test', () => {
 		const number = 2;
-		expect(number).to.equal(3);
+		expect(number).to.equal(2);
 	});
 });
