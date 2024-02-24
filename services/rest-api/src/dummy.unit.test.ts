213be0b6a874dea109c29eb9eedb2711b1de8a7a
njallskarp <njallis@gmail.com>
2024-02-24 11:21:13 -0800

    Added dummy unit test

---
+++ services/rest-api/src/dummy.unit.test.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+
+/**
+ * For `npm test` to work there needs to be at least one unit test file.
+ * 
+ * Remove this file once an actual unit test file is added.
+ * 
+ */
+
+describe('#isProductionEnvironment', function () {
+		it('should return true if NODE_ENV is set to "production"', function () {
+            expect(4).to.equal(4);
+		});
+	});
