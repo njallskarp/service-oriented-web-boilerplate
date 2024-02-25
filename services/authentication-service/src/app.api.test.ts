43cb99a7aca0c4a8b63a96f3ad8dcb7b0886f468
njallskarp <njallis@gmail.com>
2024-02-25 14:27:51 -0800

    Fixed api test

--- services/authentication-service/src/app.api.test.ts
+++ services/authentication-service/src/app.api.test.ts
@@ -18,19 +18,4 @@ describe('API Endpoints', () => {
 				});
 		});
 	});
-
-	describe('GET /api/v1/items', () => {
-		it('should return a list of items', (done) => {
-			chai.request(app)
-				.get('/api/v1/items')
-				.end((err, res) => {
-					expect(res).to.have.status(200);
-					expect(res).to.be.json;
-					expect(res.body).to.be.an('array');
-					expect(res.body.length).to.equal(3);
-					expect(res.body).to.deep.include({ name: 'Pencil', id: 1 });
-					done();
-				});
-		});
-	});
 });
